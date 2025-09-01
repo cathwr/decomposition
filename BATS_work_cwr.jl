500c18bb61f36b18fc44f2803ba42173b8a63288
Cathy Wimart-Rousseau <cathwr@Mac.Home>
2025-09-01 16:42:51 +0100

    Update:01/09/2025

--- BATS_work_cwr.jl
+++ BATS_work_cwr.jl
@@ -343,7 +343,7 @@ function gaussian(x;μ::Float64=0.0,σ::Float64)
 function decompose_temp(init_temp::Vector{Float64},final_temp::Vector{Float64}
                        ,init_DIC::Vector{Float64},final_DIC::Vector{Float64}
                        ;transient_val::Float64=0.017
-                       ,gaussian_width::Float64=0.0001)::Tuple{Vector{Float64}, Vector{Float64}}
+                       ,gaussian_width::Float64=0.0001)::Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64}}
                     
     #= 
     The gaussain width is used to switch out the more complicated decomposition 
@@ -362,8 +362,6 @@ function decompose_temp(init_temp::Vector{Float64},final_temp::Vector{Float64}
     ξ = central_diff(init_temp)
 
     κr = ξ ./ ζ
-
-
 
     weight1 = gaussian(ζ,σ = gaussian_width)
     weight2 = (weight1 .- 1.0) ./ (κr .- transient_val)
@@ -393,7 +391,7 @@ function decompose_temp(init_temp::Vector{Float64},final_temp::Vector{Float64}
     excess_temp = output_vec[1,:]
     redist_temp = output_vec[2,:]
 
-    return excess_temp, redist_temp
+    return excess_temp, redist_temp,  κr
 
 end
 
@@ -402,13 +400,14 @@ end
 
 excess_temp_profiles = Dict{Float64, Vector{Float64}}()
 redist_temp_profiles = Dict{Float64, Vector{Float64}}()
+κr_profiles = Dict{Float64, Vector{Float64}}()
 
 init_date = uniq_dates[101] # Change this if you want to use a different init_date.
 # Currently, set to the last date in 1996
 println("Using $init_date as the initial date")
 
 for date in uniq_dates
-    excess_temp_profiles[date], redist_temp_profiles[date] = decompose_temp(
+    excess_temp_profiles[date], redist_temp_profiles[date],κr_profiles[date] = decompose_temp(
         gridded_TMP[init_date], gridded_TMP[date],
         gridded_DIC[init_date], gridded_DIC[date],
         transient_val=0.017,
@@ -449,6 +448,10 @@ ylabel="Pressure (db)", title="Θredistri profiles over Time", yflip=true, color
 Plots.contour!(uniq_dates, pr_grid, profile_mat, color=:black, alpha=0.5, levels=[0.0], linewidth=2)
 savefig(P5, joinpath(git_root, "data_output", "BATS_temperature_redistri_time.png"))
 
+## Plot Kr versus time as scatter
+kr_profile = κr_profiles[uniq_dates[1]] # pick juste one date
+P8 = Plots.plot(kr_profile, pr_grid, color=:black,linewidth=5, yflip=true, legend = false, xlabel="κr_profile", ylabel="Pressure (db)", title="BATS - κr profile for year $(uniq_dates[1])")
+savefig(P8, joinpath(git_root, "data_output", "BATS_Kr_time.png"))
 
 # Plot of the DIC-Temperature values
 P6=Plots.scatter(DIC, TMP, zcolor=decimal_year, markersize=5, alpha=0.4, 
@@ -476,3 +479,138 @@ sorted_columns = [Symbol(k)=>v for (k,v) in sorted_pairs]
 
 DataFrame(;[Symbol(k)=>v for (k,v) in redist_temp_profiles]..., Pressure_db=pr_grid) |>
     CSV.write(joinpath(git_root, "output_files", "bats_redist_temp_profiles.csv"), header=true)
+
+
+
+##### TEST with anotehr Temp excess - redistri function
+function ExcessRedistTempSalFromTempSalDIC(init_temp::Vector{Float64},final_temp::Vector{Float64}
+    ,init_DIC::Vector{Float64},final_DIC::Vector{Float64}
+    ,init_Sal::Vector{Float64},final_Sal::Vector{Float64}
+    ;transient_val::Float64=0.017
+    ,zDimension::Int64=1
+    ,gaussian_width::Float64=0.0001)
+
+if size(init_temp) == size(final_temp) == size(init_DIC) == size(final_DIC) ==
+size(init_Sal) == size(final_Sal)
+nothing
+else
+error("\"init_temp\", \"final_temp\", \"init_DIC\", \"final_DIC\"
+\"init_Sal\", \"final_Sal\" must all be the same size")
+end
+
+ζ = central_diff(init_DIC)
+ξ = central_diff(init_temp)
+η = central_diff(init_Sal)
+
+κrT = ξ ./ ζ
+κrS = η ./ ζ
+τ = η ./ ξ
+
+weight1 = gaussian(ζ,σ = gaussian_width)
+weight2 = (weight1 .- 1.0) ./ (κrT .- transient_val)
+
+LENGTH_1 = size(init_temp,1)
+LENGTH_2 = size(init_temp,2)
+
+mat1 = fill(NaN,3,2,LENGTH_1,LENGTH_2)
+mat2 = fill(NaN,3,2,LENGTH_1,LENGTH_2)
+
+for i = 1:LENGTH_1, j = 1:LENGTH_2
+mat1[:,:,i,j] = [0              transient_val
+;1              -transient_val
+;τ[i,j]              -transient_val*τ[i,j]]
+
+mat2[:,:,i,j] = [transient_val -transient_val*κrT[i,j]
+;-κrT[i,j]         transient_val*κrT[i,j]
+;-κrS[i,j]         transient_val*κrS[i,j] ]
+end
+
+ΔΘ = final_temp - init_temp
+ΔDIC = final_DIC - init_DIC
+ΔSal = final_Sal - init_Sal
+
+inputVectors = fill(NaN,2,LENGTH_1,LENGTH_2);
+outputVectors = fill(NaN,3,LENGTH_1,LENGTH_2);
+inputVectors[1,:,:] = ΔΘ
+inputVectors[2,:,:] = ΔDIC
+
+for i =1:LENGTH_1, j = 1:LENGTH_2
+outputVectors[:,i,j] = (weight1[i,j] * mat1[:,:,i,j] + weight2[i,j] * mat2[:,:,i,j]) * inputVectors[:,i,j]
+end
+
+ExcessTemperature = outputVectors[1,:,:]
+RedistTemperature = outputVectors[2,:,:]
+RedistSalinity    = outputVectors[3,:,:]
+ExcessSalinity    = ΔSal - RedistSalinity
+
+return ExcessTemperature, RedistTemperature, ExcessSalinity, RedistSalinity
+end
+
+excess_temp_profiles = Dict{Float64, Matrix{Float64}}()
+redist_temp_profiles = Dict{Float64, Matrix{Float64}}()
+excess_sal_profiles = Dict{Float64, Matrix{Float64}}()
+redist_sal_profiles = Dict{Float64, Matrix{Float64}}()
+
+init_date = uniq_dates[101] # Change this if you want to use a different init_date.
+# Currently, set to the last date in 1996
+println("Using $init_date as the initial date")
+
+for date in uniq_dates
+    excess_temp_profiles[date], 
+    redist_temp_profiles[date], 
+    excess_sal_profiles[date], 
+    redist_sal_profiles[date] = ExcessRedistTempSalFromTempSalDIC(
+        gridded_TMP[init_date], gridded_TMP[date],
+        gridded_DIC[init_date], gridded_DIC[date],
+        gridded_SAL[init_date], gridded_SAL[date];
+        transient_val=0.017,
+        gaussian_width=0.0001,
+        zDimension=1
+    )
+end
+
+## Plot Temp excess (OTHER METHOD) versus time as a heatmap
+profile_mat = zeros(length(pr_grid), length(uniq_dates))
+for (i, yr) in enumerate(uniq_dates)
+    profile_mat[:, i] = excess_temp_profiles[yr]
+end
+
+P12=Plots.heatmap(uniq_dates, pr_grid, profile_mat, color=:viridis, xlabel="Year", 
+ylabel="Pressure (db)", title="BATS - Θe profiles over time", yflip=true, colorbar=true, cmap=:bwr,)
+#clim=(-0.5,0.5),)
+Plots.contour!(uniq_dates, pr_grid, profile_mat, color=:black, alpha=0.5, levels=[0.0], linewidth=2)
+savefig(P12, joinpath(git_root, "data_output", "BATS_temperature_excess_time_OTHER_FUNCTION.png"))
+## Plot Temp excess (OTHER METHOD) versus time as a heatmap
+profile_mat = zeros(length(pr_grid), length(uniq_dates))
+for (i, yr) in enumerate(uniq_dates)
+    profile_mat[:, i] = redist_temp_profiles[yr]
+end
+
+P13=Plots.heatmap(uniq_dates, pr_grid, profile_mat, color=:viridis, xlabel="Year", 
+ylabel="Pressure (db)", title="BATS - Θredistri profiles over time", yflip=true, colorbar=true, cmap=:bwr,)
+#clim=(-0.5,0.5),)
+Plots.contour!(uniq_dates, pr_grid, profile_mat, color=:black, alpha=0.5, levels=[0.0], linewidth=2)
+savefig(P13, joinpath(git_root, "data_output", "BATS_temperature_redistri_time_OTHER_FUNCTION.png"))
+
+## Plot SAL excess (OTHER METHOD) versus time as a heatmap
+profile_mat = zeros(length(pr_grid), length(uniq_dates))
+for (i, yr) in enumerate(uniq_dates)
+    profile_mat[:, i] = excess_sal_profiles[yr]
+end
+
+P14=Plots.heatmap(uniq_dates, pr_grid, profile_mat, color=:viridis, xlabel="Year", 
+ylabel="Pressure (db)", title="BATS - SAL-e profiles over time", yflip=true, colorbar=true, cmap=:bwr,)
+#clim=(-0.5,0.5),)
+Plots.contour!(uniq_dates, pr_grid, profile_mat, color=:black, alpha=0.5, levels=[0.0], linewidth=2)
+savefig(P14, joinpath(git_root, "data_output", "BATS_salinity_excess_time_OTHER_FUNCTION.png"))
+## Plot SAL excess (OTHER METHOD) versus time as a heatmap
+profile_mat = zeros(length(pr_grid), length(uniq_dates))
+for (i, yr) in enumerate(uniq_dates)
+    profile_mat[:, i] = redist_sal_profiles[yr]
+end
+
+P15=Plots.heatmap(uniq_dates, pr_grid, profile_mat, color=:viridis, xlabel="Year", 
+ylabel="Pressure (db)", title="BATS - SAL-redistri profiles over time", yflip=true, colorbar=true, cmap=:bwr,)
+#clim=(-0.5,0.5),)
+Plots.contour!(uniq_dates, pr_grid, profile_mat, color=:black, alpha=0.5, levels=[0.0], linewidth=2)
+savefig(P15, joinpath(git_root, "data_output", "BATS_salinity_redistri_time_OTHER_FUNCTION.png"))
